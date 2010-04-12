1a88b6f72d244922bbd0209faa338891da4c6707
Eric Davis <edavis@littlestreamsoftware.com>
2010-04-12 15:46:45 -0700

    Added some gems for development

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,3 +26,7 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+config.gem "edavis10-object_daddy", :lib => "object_daddy"
+config.gem 'shoulda', :lib => 'shoulda'
+config.gem 'mocha', :version => '>=0.9.7'
+config.gem 'webrat'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,14 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
+require 'shoulda'
+require 'mocha'
+require 'webrat'
+
+Webrat.configure do |config|
+  config.mode = :rails
+  config.open_error_files = false
+end
 
 class ActiveSupport::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
