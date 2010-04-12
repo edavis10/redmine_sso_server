5311bcf2c3c21ab436c9b4b629bea58b522f4f0b
Eric Davis <edavis@littlestreamsoftware.com>
2010-04-12 15:56:14 -0700

    [#3862] Hooked up a basic InheritedResources controller.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,6 @@
-class AccountsController < ApplicationController
+class AccountsController < InheritedResources::Base
+  defaults :resource_class => User, :collection_name => 'users', :instance_name => 'user'
+  
+  respond_to :xml
+  actions :create, :update, :present
 end
